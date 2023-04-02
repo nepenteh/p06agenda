43b2287ebd30b5b31e355e68346d6ae6361751bc
Jose Manuel Rosado <nepenteh@gmail.com>
2023-04-02 17:59:31 +0200

    #1 Creación de la entidad Persona

---
+++ src/Entity/Persona.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PersonaRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PersonaRepository::class)]
+class Persona
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $Nombre = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $Apellido1 = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $Apellido2 = null;
+
+    #[ORM\Column(length: 20, nullable: true)]
+    private ?string $Telefono = null;
+
+    #[ORM\Column(length: 100, nullable: true)]
+    private ?string $CorreoElectronico = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $FechaNacimiento = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->Nombre;
+    }
+
+    public function setNombre(string $Nombre): self
+    {
+        $this->Nombre = $Nombre;
+
+        return $this;
+    }
+
+    public function getApellido1(): ?string
+    {
+        return $this->Apellido1;
+    }
+
+    public function setApellido1(?string $Apellido1): self
+    {
+        $this->Apellido1 = $Apellido1;
+
+        return $this;
+    }
+
+    public function getApellido2(): ?string
+    {
+        return $this->Apellido2;
+    }
+
+    public function setApellido2(?string $Apellido2): self
+    {
+        $this->Apellido2 = $Apellido2;
+
+        return $this;
+    }
+
+    public function getTelefono(): ?string
+    {
+        return $this->Telefono;
+    }
+
+    public function setTelefono(?string $Telefono): self
+    {
+        $this->Telefono = $Telefono;
+
+        return $this;
+    }
+
+    public function getCorreoElectronico(): ?string
+    {
+        return $this->CorreoElectronico;
+    }
+
+    public function setCorreoElectronico(?string $CorreoElectronico): self
+    {
+        $this->CorreoElectronico = $CorreoElectronico;
+
+        return $this;
+    }
+
+    public function getFechaNacimiento(): ?\DateTimeInterface
+    {
+        return $this->FechaNacimiento;
+    }
+
+    public function setFechaNacimiento(?\DateTimeInterface $FechaNacimiento): self
+    {
+        $this->FechaNacimiento = $FechaNacimiento;
+
+        return $this;
+    }
+}
